d0290ceb4f7bb64eb42d8e1598f518d19e41435a
Robert Plummer <RobertLeePlummerJr@Gmail.com>
2015-05-27 12:56:58 -0400

    Create base path constant

--- modules/setup/run.php
+++ modules/setup/run.php
@@ -1,6 +1,9 @@
 <?php
 
 $thisDir = dirname(__FILE__);
+
+define('path', dirname(dirname($thisDir)));
+
 require_once $thisDir . '/../../vendor/autoload.php';
 if (file_exists($thisDir . '/config.php')) {
 	require_once $thisDir . '/config.php';
