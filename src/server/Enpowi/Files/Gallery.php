d2276720d437219c68e8871d78882966c249edad
Robert Plummer <RobertLeePlummerJr@Gmail.com>
2015-06-30 11:32:35 -0400

    Added created and ensure bean converts into object

--- src/server/Enpowi/Files/Gallery.php
+++ src/server/Enpowi/Files/Gallery.php
@@ -11,21 +11,47 @@ class Gallery
     public $name;
     public $description;
     public $userID;
+	public $created;
     public $pictures = [];
 
     private $_bean = null;
 
     public function __construct($id = null, $bean = null)
     {
-        if ($bean === null) {
-            if ($id === null) {
-                throw new \Exception('Need bean or id');
-            }
-            $bean = $this->_bean = R::findOne('gallery', ' id = :id ', ['id' => $id]);
-        } else {
-            $this->_bean = $bean;
-        }
+	    if ($bean === null && $id !== null) {
+		    $this->_bean = R::findOne('gallery', ' id = :id ', ['id' => $id]);
+	    } else {
+		    $this->_bean = $bean;
+	    }
+
+	    if ($this->_bean === null) {
+		    throw new \Exception('Need bean or id');
+	    }
+
+	    $this->convertFromBean();
     }
+
+	public function exists()
+	{
+		if ($this->_bean === null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	private function convertFromBean()
+	{
+		$bean = $this->_bean;
+
+		if (!$this->exists()) return $this;
+
+		$this->name = $bean->name;
+		$this->description = $bean->description;
+		$this->created = $bean->created;
+
+		return $this;
+	}
 
     public function setName($value)
     {
@@ -59,6 +85,7 @@ class Gallery
         if ($this->_bean === null) {
             $bean = R::dispense('gallery');
             $bean->userID = App::user()->id();
+	        $bean->created = R::isoDateTime();
             $this->_bean = $bean;
         }
         return $this->_bean;
@@ -95,7 +122,10 @@ class Gallery
     public static function create($name, $description)
     {
         $bean = R::dispense('gallery');
+	    $bean->userID = App::user()->id();
+	    $bean->created = R::isoDateTime();
         $gallery = new Gallery(null, $bean);
+
         return $gallery
             ->setName($name)
             ->setDescription($description)
