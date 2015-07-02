19382732d3d50871cae087842a9ccf7c3ea70edb
Robert Plummer <RobertLeePlummerJr@Gmail.com>
2015-07-02 07:44:27 -0400

    Added event for changing Email

---
+++ src/server/Enpowi/Event/User/UpdateEmail.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robert
+ * Date: 7/2/15
+ * Time: 7:37 AM
+ */
+
+namespace Enpowi\Event\User;
+
+use Enpowi\Event;
+
+class UpdateEmail extends Event\Base {
+
+}
