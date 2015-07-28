4e0e7f5df3a9f6109126b7fde70401454611c481
robertleeplummerjr <Adminuser1>
2015-07-28 13:34:28 -0400

    remember

--- modules/user/login.php
+++ modules/user/login.php
@@ -9,12 +9,16 @@ $response = [];
 
 $user = User::getByEmailAndPassword(App::param('email'), App::param('password'));
 
+
 if ($user === null) {
 	$response['paramResponse'] = ['email'=>'Invalid email or password'];
 	$response['id'] = -1;
 } else {
 	$login = $user->login();
 	$response['id'] = $user->id;
+    if (App::paramIs('remember')) {
+        App::get()->authentication->rememberUserId();
+    }
 }
 
 echo json_encode($response);
