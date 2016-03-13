4bb1c234d6527ff1b2e869550eb956d26e0de093
Robert Plummer <robertleeplummerjr@gmail.com>
2016-03-12 21:38:13 -0500

    Fixed a bug where v-find and listen were not working well together

--- src/client/Enpowi/directives.js
+++ src/client/Enpowi/directives.js
@@ -62,8 +62,13 @@ Namespace('Enpowi').
                                 }
 
                                 form = parent;
-
-                                Enpowi.utilities.trigger(form, 'submit');
+                                /*
+                                 this timeout is here so that all event handlers that happen after this event get to
+                                 finish before the form has submit triggered
+                                */
+                                setTimeout(function() {
+                                  Enpowi.utilities.trigger(form, 'submit');
+                                }, 0);
                             };
 
                         el.addEventListener('change', listenFn);
