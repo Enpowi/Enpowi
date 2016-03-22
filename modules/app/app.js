ec222cdcd2b29bf5a25a2439ccb9db2176e2938a
Robert Plummer <robertleeplummerjr@gmail.com>
2016-03-22 08:45:40 -0400

    reformat and null check

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,41 +1,42 @@
 $.holdReady(true);
 
 Enpowi.App.subTo()
-	._continue(function() {
-		$.holdReady(false);
-		if (app.modal) app.modal.modal('hide');
-	})
-	.delay(function() {
-		app.modal = $('<div>').modal('show');
-	})
-	.deny(function() {
-		app.go('#/');
-		return false;
-	});
+  ._continue(function () {
+    $.holdReady(false);
+    if (app.modal) app.modal.modal('hide');
+  })
+  .delay(function () {
+    app.modal = $('<div>').modal('show');
+  })
+  .deny(function () {
+    app.go('#/');
+    return false;
+  });
 
-var app = new Enpowi.App(function(html) {
-    var el = Enpowi.directives.defaultModuleElement;
-    while (el.firstChild !== null) {
-        el.removeChild(el.firstChild);
-    }
-    el.appendChild(html);
+var app = new Enpowi.App(function (html) {
+  var el = Enpowi.directives.defaultModuleElement;
+	if (el === null) return;
+  while (el.firstChild !== null) {
+    el.removeChild(el.lastChild);
+  }
+  el.appendChild(html);
 });
 
-app.load('modules/default/loading.html', function(html){
-	app.loadingElement = html;
+app.load('modules/default/loading.html', function (html) {
+  app.loadingElement = html;
 });
 
 new Vue({
-	el: document.body
+  el: document.body
 });
 
-$(document).keydown(function(e) {
-	if (e.ctrlKey) {
-		switch (e.key) {
-			case 'U':
-			case 'u':
-				window.open(Enpowi.utilities.url(app.hasher.getHash()));
-				return false;
-		}
-	}
+$(document).keydown(function (e) {
+  if (e.ctrlKey) {
+    switch (e.key) {
+      case 'U':
+      case 'u':
+        window.open(Enpowi.utilities.url(app.hasher.getHash()));
+        return false;
+    }
+  }
 });
